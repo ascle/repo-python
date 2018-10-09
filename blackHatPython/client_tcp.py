c8c933012ada308d689f554ba5392ce04c786d4c
ascle <user@host.host>
2018-10-08 23:40:50 -0300

    dsfsd

--- blackHatPython/client_tcp.py
+++ blackHatPython/client_tcp.py
@@ -1,7 +1,8 @@
 import socket
+import signal
 
-target_host = 'www.googe.com'
-target_port = 80
+target_host = '0.0.0.0'
+target_port = 9999
 
 # Cria um objeto socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,10 +10,12 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Faz o cliente se conectar
 client.connect((target_host, target_port))
 
+time.sleep(1)
+
 # Envia alguns dados
-client.send('GET / HTTP/1.1\r\nHOST:google.com\r\n\r\n')
+client.send('GET / HTTP/1.1\r\nHOST:google.com\r\n\r\n'.encode())
 
 # Recebe alguns dados
 response = client.recv(4096)
 
-print response
+print (response)
---
+++ blackHatPython/serv_tcp.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+# Esta é a thread para tratamento de clientes
+def handle_client(client_socket):
+  request = client_socket.recv(1024)
+  print ('[*] Received: %s' %request)
+  #Envia o pacote de volta
+  client_socket.send('ACK!')
+  client_socket.close()
+  
+bind_ip = '0.0.0.0'
+bind_porta = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Endereço e porta q queremos q o servidor fique ouvindo
+server.bind((bind_ip, bind_porta))
+# Comece escultar, no maximo 5 conexoes
+server.listen(5)
+print ('[*] Listening on: %s:%d' %(bind_ip, bind_porta))
+
+while True:
+  # Cliente se conecta
+  client, addr = server.accept()
+  print ('[*] Accepted cooection from: %s:%d' %(addr[0], addr[1]))
+  
+  #coloca nossa thread de cliente em ação para tratar dados de entrada
+  client_handler = threading.Thread(target=handle_client, args=(client,))
+  client_handler.start
+
+
+    
